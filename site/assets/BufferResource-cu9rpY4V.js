606d0fe503b18e8f148ad2ee86857a8d98215e9d
Your Name <elias_afram12@hotmail.com>
2026-09-01 22:59:26 +0200

    deploy atomic debuff persistence

--- site/assets/BufferResource-cu9rpY4V.js
+++ site/assets/BufferResource-cu9rpY4V.js
@@ -1,4 +1,4 @@
-import{A as _,D as y,S as c,G as S,H as g,J as m,E as I,K as p}from"./index-DaJ69HD6.js";const M={name:"texture-bit",vertex:{header:`
+import{A as _,D as y,S as c,G as S,H as g,J as m,E as I,K as p}from"./index-DXzWU6x-.js";const M={name:"texture-bit",vertex:{header:`
 
         struct TextureUniforms {
             uTextureMatrix:mat3x3<f32>,
